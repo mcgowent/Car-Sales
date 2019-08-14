736ce4e6b13946669e926b0997ae595018b56585
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-14 13:48:58 -0700

    Setting up the redux store and reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from "redux"
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+function reducer() {
+    return {
+        title: "This is Example Text"
+    }
+}
+
+const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
